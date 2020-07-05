940ac12a40dfd55a1d668203d6d6b299a093b02c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-05 11:18:55 -0400

    Increment cart using v-on

--- main.js
+++ main.js
@@ -6,8 +6,22 @@ let app = new Vue({
     inStock: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
-      { variantId: 2234, variantColor: "green" },
-      { variantId: 2235, variantColor: "blue" },
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite",
+      },
     ],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
   },
 });
