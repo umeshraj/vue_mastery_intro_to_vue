35d0ef00716c53ee8a8511f83ea3d42a12ebaa28
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-05 11:22:42 -0400

    Using ES6 format instead of anonymous functions

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ let app = new Vue({
     cart: 0,
   },
   methods: {
-    addToCart: function () {
+    addToCart() {
       this.cart += 1;
     },
-    updateProduct: function (variantImage) {
+    updateProduct(variantImage) {
       this.image = variantImage;
     },
   },
