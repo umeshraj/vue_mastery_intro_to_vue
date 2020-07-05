9b179b0ca32b527c82203d225f8e537276a12a65
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-05 11:03:39 -0400

    v-for

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let app = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
-    inventory: 9,
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
   },
 });
