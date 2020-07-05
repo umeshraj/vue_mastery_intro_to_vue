61d6307e87c875ac284dc6f6eca7c1f96798b88e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-05 10:24:40 -0400

    Homework binding href attribute to url

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ let app = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
+    link:
+      "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
   },
 });
