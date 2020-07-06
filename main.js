c331370faeba12bc6052d8f74d372bd9c84c248b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-06 08:18:00 -0400

    Convert inStock property to a computed property

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ let app = new Vue({
   data: {
     brand: "Vue Mastery",
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
-    inStock: false,
+    selectedVariant: 0,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
         variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10,
       },
       {
         variantId: 2235,
         variantColor: "blue",
         variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0,
       },
     ],
     cart: 0,
@@ -24,13 +25,20 @@ let app = new Vue({
     addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      // console.log(index);
+      this.selectedVariant = index;
     },
   },
   computed: {
     title() {
       return this.brand + "  " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
     },
   },
 });
