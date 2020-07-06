0c0f91a0a6d79566ac387fdc6a586d045f118ec5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-06 07:05:16 -0400

    Example of a computed property

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
     inStock: false,
@@ -25,6 +26,11 @@ let app = new Vue({
     },
     updateProduct(variantImage) {
       this.image = variantImage;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + "  " + this.product;
     },
   },
 });
