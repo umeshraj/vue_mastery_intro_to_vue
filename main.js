5d75d3ed53b719b3a5cdd03481fc97219cedda87
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-07 07:57:44 -0400

    Adding review

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ Vue.component("product", {
       Add to Cart
     </button>
 
+  <product-review @review-submitted="addReview"></product-review>
+
   </div>
 </div>`,
   data() {
@@ -60,6 +62,7 @@ Vue.component("product", {
           variantQuantity: 0,
         },
       ],
+      reviews: [],
     };
   },
   methods: {
@@ -69,6 +72,9 @@ Vue.component("product", {
     updateProduct(index) {
       // console.log(index);
       this.selectedVariant = index;
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     },
   },
   computed: {
@@ -83,6 +89,58 @@ Vue.component("product", {
     },
     shipping() {
       return this.premium ? "Free" : 2.99;
+    },
+  },
+});
+
+Vue.component("product-review", {
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+  <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" placeholder="name">
+  </p>
+  
+  <p>
+    <label for="review">Review:</label>      
+    <textarea id="review" v-model="review"></textarea>
+  </p>
+  
+  <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+  </p>
+      
+  <p>
+    <input type="submit" value="Submit">  
+  </p>    
+
+</form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+    };
+  },
+  methods: {
+    onSubmit() {
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+      };
+      this.$emit("review-submitted", productReview);
+      this.name = null;
+      this.review = null;
+      this.rating = null;
     },
   },
 });
