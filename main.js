671b14905069af55d3a5adbfe113fb49eca26bd3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-07 08:07:26 -0400

    Form validation

--- main.js
+++ main.js
@@ -108,6 +108,14 @@ Vue.component("product", {
 Vue.component("product-review", {
   template: `
   <form class="review-form" @submit.prevent="onSubmit">
+  
+  <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+    <li v-for="error in errors">{{error}}</li>
+    </ul>
+  </p>
+
   <p>
     <label for="name">Name:</label>
     <input id="name" v-model="name" placeholder="name">
@@ -140,19 +148,26 @@ Vue.component("product-review", {
       name: null,
       review: null,
       rating: null,
+      errors: [],
     };
   },
   methods: {
     onSubmit() {
-      let productReview = {
-        name: this.name,
-        review: this.review,
-        rating: this.rating,
-      };
-      this.$emit("review-submitted", productReview);
-      this.name = null;
-      this.review = null;
-      this.rating = null;
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+        };
+        this.$emit("review-submitted", productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) this.errors.push("Name requied");
+        if (!this.review) this.errors.push("Review requied");
+        if (!this.rating) this.errors.push("Rating requied");
+      }
     },
   },
 });
